7f4f35fccd50b1aaea8c7c02ca67dd6e880a059e
ssshivm670 <ssshivam670@gmail.com>
2022-02-08 12:47:21 +0530

    Write stream example

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
 
+// read stream
 const readStream = fs.createReadStream('largeText.txt', {
     encoding: 'utf-8'
 });
 
+// readStream.on('data',chunk=> { 
+//     console.log('######### new chunk #######');
+//     console.log(chunk);
+// })
+
+//write stream
+
+const writeStream = fs.createWriteStream('writeStream.txt');
+
 readStream.on('data',chunk=> { 
     console.log('######### new chunk #######');
     console.log(chunk);
+
+    writeStream.write('\n ###### new chuck #######');
+    writeStream.write(chunk)
 })

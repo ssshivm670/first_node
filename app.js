2f59edd2a4016b301b57ae320c321a32d91e4253
ssshivm670 <ssshivam670@gmail.com>
2022-02-08 12:30:46 +0530

    Create a read stream

--- app.js
+++ app.js
@@ -1,68 +1,10 @@
 const fs = require('fs');
 
-//read file 
-// fs.readFile('./files/note.txt', (err, data)=>{
-//     if(err) {
-//         console.log(23444);
-//     } else {
-//         console.log(data.toString());
-//     }
-// });
+const readStream = fs.createReadStream('largeText.txt', {
+    encoding: 'utf-8'
+});
 
-//write
-// fs.writeFile('./files/note.txt','new message', (err, data)=>{
-//     if(err) {
-//         console.log(23444);
-//     } else {
-//         console.log('new message created');
-//     }
-// })
-
-// append file
-
-// fs.appendFile('./files/note.txt','\nnew message', (err, data)=>{
-//     if(err) {
-//         console.log(23444);
-//     } else {
-//         console.log('new message created');
-//     }
-// })
-
-//create folder 
-// if(!fs.existsSync('./files/newfolder')){
-//     fs.mkdir('./files/newfolder',(err)=> {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log('new folder created');
-//         }
-//     })
-// }else {
-//     console.log('folder exist');
-// }
-
-// delete folder 
-// if(fs.existsSync('./files/newfolder')){
-//     fs.rmdir('./files/newfolder',(err)=> {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log('deleted');
-//         }
-//     })
-// }else {
-//     console.log('folder does not exist');
-// }
-
-//delete file 
-// if(fs.existsSync('./files/note.txt')){
-//     fs.unlink('./files/note.txt',(err)=> {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log(' file deleted');
-//         }
-//     })
-// }else {
-//     console.log('file does not exist');
-// }
+readStream.on('data',chunk=> { 
+    console.log('######### new chunk #######');
+    console.log(chunk);
+})

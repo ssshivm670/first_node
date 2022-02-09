b320719fb5040ab0a87aca6e06e28a2ff7a11e26
ssshivm670 <ssshivam670@gmail.com>
2022-02-09 17:12:38 +0530

    use response object

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const port = 3000;
 const hostname = 'localhost'
 
 const server = http.createServer((req, res)=>{
-    console.log('request url', req.url);
-    console.log('request method', req.method);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<body class="app"></body>')
+    res.write('<h3>welcome to haveli</h3>');
+    res.write('<p>run for life</p>');
+    res.end();
 });
 
 server.listen(port, hostname, ()=>{

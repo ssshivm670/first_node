8372e6c2360aee9c5a7e97f430a57c26d8030eea
ssshivm670 <ssshivam670@gmail.com>
2022-02-09 17:30:32 +0530

    creating basic routes without expressJS

--- server.js
+++ server.js
@@ -4,18 +4,35 @@ const fs = require('fs');
 const port = 3000;
 const hostname = 'localhost'
 
-const server = http.createServer((req, res)=>{
-    res.setHeader('Content-Type', 'text/html'); 
-    fs.readFile('./view/index.html',(err,data)=>{
-        if(err){
-            console.log('Ooh nooooo',err);
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+
+
+    let route = "./views";
+
+    switch (req.url) {
+        case '/':
+            route += "index.html";
+            break;
+        case '/contact':
+            route += "/contact.html";
+            break;
+        default:
+            route += "/404.html";
+            break;
+    }
+
+    fs.readFile(route, (err, data) => {
+        if (err) {
+            console.log('Ooh nooooo', err);
             res.end();
-        }else {
+        } else {
             res.end(data);
         }
     });
 });
 
-server.listen(port, hostname, ()=>{
+server.listen(port, hostname, () => {
     console.log(`listening on port ${port}`);
 })

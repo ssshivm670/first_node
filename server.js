b7e0f11ebcd4f4b9525885f286404e114768d20b
ssshivm670 <ssshivam670@gmail.com>
2022-02-09 17:07:21 +0530

    Creating a basic server

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const port = 3000;
+const hostname = 'localhost'
+
+const server = http.createServer((req, res)=>{
+    console.log('A request is made');
+});
+
+server.listen(port, hostname, ()=>{
+    console.log(`listening on port ${port}`);
+})

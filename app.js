ae5758d024f3f9a47e14e456f8d8f5a0523f98f5
ssshivm670 <ssshivam670@gmail.com>
2022-02-12 17:19:50 +0530

    Connecting to mongo db using mongoose

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const mongodb = 'mongodb+srv://admin:admin123@firstnode.g36tb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-app.set('view engine', 'ejs');
-app.listen(3000);
+mongoose.connect(mongodb,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTED");
+        app.listen(3000);
+    })
+    .catch((err) => console.log(err))
 
-app.get('/', (req,res)=>{
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
     const items = [
-        {name: 'mobile phone', price: 1000},
-        {name: 'book', price: 10},
-        {name: 'computer', price: 1500},
+        { name: 'mobile phone', price: 1000 },
+        { name: 'book', price: 10 },
+        { name: 'computer', price: 1500 },
     ]
-    res.render('index', { items});
+    res.render('index', { items });
 })
 
-app.get('/add-item', (req,res)=>{
+app.get('/add-item', (req, res) => {
     res.render('add-item');
 })
 
 
 //Always add in the bottom coz if added before any route then that page will be inaccessible
-app.use((req, res)=>{
+app.use((req, res) => {
     res.render('error');
 
 })

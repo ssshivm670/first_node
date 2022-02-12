36afafaa04034b19ea60a1daaf97e2f4b56e7835
ssshivm670 <ssshivam670@gmail.com>
2022-02-12 13:58:44 +0530

    Updating static html pages to ejs pages

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 
 const app = express();
-
+app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req,res)=>{
-    res.sendFile('./views/index.html', { root : __dirname});
+    res.render('index');
 })
 
 app.get('/add-item', (req,res)=>{
-    res.sendFile('./views/add-item.html', { root : __dirname});
+    res.render('add-item');
 })
 
 
 //Always add in the bottom coz if added before any route then that page will be inaccessible
 app.use((req, res)=>{
-    res.sendFile('./views/error.html', { root : __dirname});
+    res.render('error');
 
 })

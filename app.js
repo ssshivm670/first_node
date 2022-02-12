96ed053d2d61374fd345bdb6605c32f999c071ef
ssshivm670 <ssshivam670@gmail.com>
2022-02-12 17:33:12 +0530

    Create and read operation on MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Items = require('./models/items');
 
 const app = express();
 
@@ -14,6 +15,29 @@ mongoose.connect(mongodb,
     .catch((err) => console.log(err))
 
 app.set('view engine', 'ejs');
+
+// Create new entry in mondo db
+app.get('/create-item',(req,res)=> {
+    const item = new Items({
+        name: 'phone',
+        price: 1000
+    });
+    item.save()
+        .then(result=> res.send(result));
+})
+
+// Get all entries from mongodb
+app.get('/get-items',(req,res)=>{
+    Items.find()
+        .then(result=>res.send(result));
+});
+
+
+//Get item by ID from mongoDB
+app.get('/get-item',(req,res)=>{
+    Items.findById('6207a15c60386e37382a41bb')
+        .then((result)=>res.send(result))
+});
 
 app.get('/', (req, res) => {
     const items = [

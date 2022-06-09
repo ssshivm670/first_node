e55f3af0df01e369dd02faece647ab5a16999932
Shivam Sharma <C480013@cttlc.com>
2022-06-09 11:32:56 +0530

    Added update and delete funtionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,109 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Preloader from "./components/Preloader";
-import { readTodos } from "./functions";
+import { createTodo, deleteTodo, readTodos, updateTodo } from "./functions";
 
 
 function App() {
+
+  const [todo, setTodo] = useState({ title: '', content: '' });
+  const [todos, setTodos] = useState(null);
+  const [currentId, setCurrentId] = useState(0);
+  useEffect(() => {
+    let currentTodo = currentId !== 0 ? todos.find(todo => todo._id === currentId) : { title: '', content: '' }
+    setTodo(currentTodo)
+  }, [currentId])
   useEffect(() => {
     const fetchData = async () => {
       const result = await readTodos();
-      console.log(result);
+      setTodos(result)
     }
     fetchData()
+  }, [currentId])
+  const clear = () => {
+    setCurrentId(0);
+    setTodo({ title: '', content: '' });
+  }
+  useEffect(() => {
+    const clearField = (e) => {
+      if (e.keyCode === 27) {
+        clear()
+      }
+    }
+    window.addEventListener('keydown', clearField)
+    return () => window.removeEventListener('keydown', clearField)
   }, [])
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    if (currentId === 0) {
+      const result = await createTodo(todo)
+      setTodos([...todos, result])
+      clear();
+    } else {
+      await updateTodo(currentId, todo)
+      clear();
+    }
+  }
+  const removeTodo = async (e, id) => {
+    e.preventDefault();
+    await deleteTodo(id);
+    const todosCopy = [...todos];
+    todosCopy.filter(todo => todo._id !== id);
+    setTodos(todosCopy);
+    clear();
+  }
   return (
     <div className="container">
       <div className="row">
-        <form className="col s12">
+        <form className="col s12" onSubmit={onSubmitHandler}>
           <div className="row">
             <div className="input-field col s6">
               <i className="material-icons prefix">title</i>
-              <input id="icon_prefix" type="text" className="validate" />
+              <input id="icon_prefix" type="text" className="validate"
+                value={todo.title}
+                onChange={e => setTodo({ ...todo, title: e.target.value })}
+              />
               <label htmlFor="icon_prefix">Title</label>
             </div>
             <div className="input-field col s6">
               <i className="material-icons prefix">description</i>
-              <input id="description" type="tel" className="validate" />
+              <input id="description" type="tel" className="validate"
+                value={todo.content}
+                onChange={e => setTodo({ ...todo, content: e.target.value })}
+              />
               <label htmlFor="description">Content</label>
             </div>
           </div>
+          <div className="row right-align">
+            <button className="waves-effect waves-light btn">Submit</button>
+          </div>
         </form>
-        <Preloader />
-        <div class="collection">
-          <a href="#!" class="collection-item">Alvin</a>
-          <a href="#!" class="collection-item active">Alvin</a>
-          <a href="#!" class="collection-item">Alvin</a>
-          <a href="#!" class="collection-item">Alvin</a>
-        </div>
+        {
+          !todos ? <Preloader /> : todos.length > 0 ? <ul className="collection">
+            {todos.map(todo => (
+              <li key={todo._id}
+                onClick={() => setCurrentId(todo._id)}
+                className="collection-item">
+                  <div>
+                    <h5>{todo.title}</h5>
+                    <p>{todo.content}
+                      <a href="#!"
+                        onClick={(e) =>
+                          removeTodo(e, todo._id)} 
+                        className="secondary-content"
+                        >
+                          <i className="material-icons">
+                            delete
+                          </i>
+                      </a>
+                    </p>
+                  </div>
+              </li>
+            ))}
+
+          </ul> : <div><h5>Nothing to do</h5></div>
+        }
+
+
       </div>
 
     </div>
